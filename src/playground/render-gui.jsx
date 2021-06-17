2e2269b359f5d6f3b71214a364653b8e43b30d0b
Shinichiro Oba <bricklife@gmail.com>
2021-06-17 21:03:32 +0900

    Disable unnecessary functions

--- src/playground/render-gui.jsx
+++ src/playground/render-gui.jsx
@@ -75,11 +75,7 @@ export default appTarget => {
             /> :
             <WrappedGui
                 canEditTitle
-                backpackVisible
-                showComingSoon
-                backpackHost={backpackHost}
                 canSave={false}
-                onClickLogo={onClickLogo}
             />,
         appTarget);
 };
